ae427bc97ad12c533aba2954c88e9a6d93447cd2
biven <biven@DESKTOP-C663FDP>
2021-09-02 00:06:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_1___Simple_Console_Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Miami!");
+        }
+    }
+}
